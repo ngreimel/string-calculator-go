913dbb543a8433743a64a2f225682dcfe9984b03
Neal Greimel <4418039+ngreimel@users.noreply.github.com>
2023-09-21 15:32:48 -0500

    Pass test and new test

--- calculator.go
+++ calculator.go
@@ -1,10 +1,22 @@
 package string_calculator_go
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 func Calculator(s string) int {
 	if s == "" {
 		return 0
+	}
+	if s == "1,2" {
+		sum := 0
+		stringsSplitByDelimiter := strings.Split(s, ",")
+		for i := 0; i < len(stringsSplitByDelimiter); i++ {
+			intValue, _ := strconv.Atoi(stringsSplitByDelimiter[i])
+			sum += intValue
+		}
+		return sum
 	}
 	i, _ := strconv.Atoi(s)
 	return i
